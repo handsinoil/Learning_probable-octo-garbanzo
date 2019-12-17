66d0d70c56586ae324cdf985dcfe612cd6972614
handsinoil <handsinoil@gmail.com>
2019-12-17 20:24:15 +0400

    угол между векторами(блокнот) wrong answer

--- codeforse/angle_vectors.py
+++ codeforse/angle_vectors.py
@@ -1,17 +1,17 @@
-import decimal
-from math import sqrt, acos
+from decimal import Decimal
+from math import sqrt, acos, fabs
 
 with open('angle2.in') as angle2in:
-	x1, y1, x2, y2 =[float(i) for i in angle2in.readline().split()]
+	x1, y1, x2, y2 = [Decimal(i) for i in angle2in.readline().split()]
 
 
-print(x1, y1, x2, y2 )
+print(x1/y1, x2/y2 )
 def scalar(x1, y1, x2, y2):
-	return abs(x1 * x2 + y1 * y2)
+	return x1 * x2 + y1 * y2
 
 
 def module(x, y):
-	return sqrt(x ** 2 + y ** 2)
+	return Decimal(sqrt(x ** 2 + y ** 2))
 
 
 cos = scalar(x1, y1, x2, y2) / (module(x1, y1) * module(x2, y2))
@@ -19,4 +19,4 @@ cos = scalar(x1, y1, x2, y2) / (module(x1, y1) * module(x2, y2))
 ang = acos(cos)
 
 with open('angle2.out', mode='wt') as o:
-	print('{:.5}'.format(ang), file=o)
+	print('{:7.5f}'.format(ang), file=o)
