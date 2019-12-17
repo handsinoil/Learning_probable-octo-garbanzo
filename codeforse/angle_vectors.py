e9fe28b164f72cd1612dfb02a9af29d9f1a21b23
handsinoil <handsinoil@gmail.com>
2019-12-17 19:23:13 +0400

    угол между векторами частично

---
+++ codeforse/angle_vectors.py
@@ -0,0 +1,24 @@
+import decimal
+from math import sqrt, acos
+
+with open('angle2.in') as angle2in:
+	x1, y1, x2, y2 =[float(i) for i in angle2in.readline().split()]
+
+
+print(x1, y1, x2, y2 )
+def scalar(x1, y1, x2, y2):
+	return x1 * x2 + y1 * y2
+
+
+def module(x, y):
+	return sqrt(x ** 2 + y ** 2)
+
+
+cos = scalar(x1, y1, x2, y2) / (module(x1, y1) * module(x2, y2))
+
+ang = acos(cos)
+
+a = decimal.Decimal(ang)
+
+with open('angle2.out', mode='wt') as o:
+	print('{:.5}'.format(a), file=o)
